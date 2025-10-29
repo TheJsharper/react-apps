dfe143f5a836d0c7b907a9dad7980c79857e947e
Pedro Salomon <pedro.salomon@gmail.com>
2025-10-29 20:35:40 +0100

    added routes app

--- heroes-app/src/router/app.routes.tsx
+++ heroes-app/src/router/app.routes.tsx
@@ -1,27 +1,45 @@
+import { AdminLayout } from "@/admin/layout/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layout/HeroesLayout";
 import { HeroPages } from "@/heroes/pages/hero/HeroPages";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/SearchPage";
+//import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 
+const SearchPage = lazy(()=>  import('@/heroes/pages/search/SearchPage').then(module => ({ default: module.SearchPage})))
 export const appRouter = createBrowserRouter(
     [
+
         {
-            path:'/',
-            element: <HomePage/>
+            path: '/',
+            element: <HeroesLayout />,
+            children: [
+                {
+                    index: true,
+                    element: <HomePage />
+                },
+                {
+                    path: 'heroes/1',
+                    element: <HeroPages />
+                },
+                {
+                    path: 'search',
+                    element: <SearchPage />
+                },
+            ]
         },
+
+
         {
-            path:'/heroes/1',
-            element: <HeroPages/>
-        },
-        {
-            path:'/search',
-            element: <SearchPage/>
-        },
-        
-        {
-            path:'/admin',
-            element: <AdminPage/>
+            path: '/admin',
+            element: <AdminLayout />,
+            children: [
+                {
+                    index: true,
+                    element: <AdminPage />
+                }
+            ]
         },
 
     ]
