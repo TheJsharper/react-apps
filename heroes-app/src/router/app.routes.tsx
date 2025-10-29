b9e47c9f02afa064e8ef1f40b05b8d0ece52e3cb
Pedro Salomon <pedro.salomon@gmail.com>
2025-10-29 21:16:21 +0100

    heroes component added

--- heroes-app/src/router/app.routes.tsx
+++ heroes-app/src/router/app.routes.tsx
@@ -1,7 +1,7 @@
 import { AdminLayout } from "@/admin/layout/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layout/HeroesLayout";
-import { HeroPages } from "@/heroes/pages/hero/HeroPages";
+import  HeroPages  from "@/heroes/pages/hero/HeroPages";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 //import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { lazy } from "react";
